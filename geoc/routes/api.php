6ef4f6923fcbf5461816faf1638029f3ab7480fa
Eliza Lima <elizapaoliello@outlook.com>
2024-05-22 10:18:34 -0300

    CRUD usuários api

--- geoc/routes/api.php
+++ geoc/routes/api.php
@@ -21,6 +21,7 @@ Route::middleware('auth:sanctum')->group(function () {
       return $request->user();
    });
    Route::post('/logout', [AuthController::class, 'logout']);
+   Route::apiResource('/users', \App\Http\Controllers\Api\UserController::class);
 });
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/signup', [AuthController::class, 'signup']);
--- geoc/site/src/assets/views/Users.jsx
+++ geoc/site/src/assets/views/Users.jsx
@@ -1,4 +1,26 @@
+import { useEffect, useState } from "react";
+import axiosClient from "../../axios-client";
+
 export default function Users() {
+   const [users, setUser] = useState([])
+   const [loading, setLoading] = useState(false)
+
+   useEffect(() => {
+      getUsers()
+   }, [])
+
+   const getUsers = () => {
+      setLoading(true)
+      axiosClient.get('/users')
+         .then(({ data }) => {
+            setLoading(true)
+            console.log(data)
+         })
+         .catch(() => {
+            setLoading(false)
+         })
+   }
+
    return (
       <div>
          Users
